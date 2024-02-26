f1869f46da23cc9603046c962c2867d8f95eb1a7
Jan Jutaszek <janjuraszek90@gmail.com>
2024-02-26 21:13:33 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const PORT = 3000;
+const path = require("path")
+const fs = require("fs")
+
+app.use(express.static('static'))
+app.use(express.static('static/cwiczenia/lekcja1'))
+app.use(express.static('static/cwiczenia/lekcja2'))
+
+app.listen(PORT, function () {
+  console.log("start serwera na porcie " + PORT)
+})
+
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/static/cwiczenia/index.html"))
+
+})
+
+app.get("/test", (req, res) => {
+  const filenames = fs.readdirSync(__dirname + "/static/cwiczenia/lekcja1");
+  const filenames2 = fs.readdirSync(__dirname + "/static/cwiczenia/lekcja2");
+  console.log(filenames)
+  console.log(filenames2);
+  res.json({ filenames, filenames2 })
+})
