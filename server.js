5328c94f82fbb8e5fb3293c066dcc56817a74d6f
JJAROX <152895376+JJAROX@users.noreply.github.com>
2024-03-18 10:50:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express")
 const app = express()
 const PORT = 3000;
 const path = require("path")
-const fs = require("fs")
+const fs = require("fs");
+const { log } = require("console");
 
 app.use(express.static('static'))
 app.use(express.static('static/cwiczenia/lekcja1'))
 app.use(express.static('static/cwiczenia/lekcja2'))
 app.use(express.static('static/cwiczenia/lekcja3'))
+app.use(express.static('static/cwiczenia/lekcja4'))
 
 app.listen(PORT, function () {
   console.log("start serwera na porcie " + PORT)
@@ -23,7 +25,7 @@ app.get("/test", (req, res) => {
   const filenames = fs.readdirSync(__dirname + "/static/cwiczenia/lekcja1");
   const filenames2 = fs.readdirSync(__dirname + "/static/cwiczenia/lekcja2");
   const filenames3 = fs.readdirSync(__dirname + "/static/cwiczenia/lekcja3");
-  console.log(filenames)
-  console.log(filenames2);
-  res.json({ filenames, filenames2, filenames3 })
+  const filenames4 = fs.readdirSync(__dirname + "/static/cwiczenia/lekcja4");
+  console.log(filenames4);
+  res.json({ filenames, filenames2, filenames3, filenames4 })
 })
